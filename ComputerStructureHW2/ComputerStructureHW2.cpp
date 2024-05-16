05a7eaec5a723bcdae6251a26c1aa0efd82e839b
kooyeongmin <rn9dud0@naver.com>
2024-05-16 14:07:24 +0900

    파싱 후, type 구분

--- ComputerStructureHW2/ComputerStructureHW2.cpp
+++ ComputerStructureHW2/ComputerStructureHW2.cpp
@@ -1,53 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 void printBinary(unsigned int num) {
     for (int i = 31; i >= 0; i--) {
         printf("%d", (num >> i) & 1);
     }
 }
-// test commit
+
+// 명령어 타입을 판별하고 출력하는 함수
+void classifyInstruction(uint32_t instruction) {
+    uint32_t opcode = instruction >> 26; // 상위 6비트 추출
+    printf("\t[Instruction Decode] ");
+    if (opcode == 0) {
+        printf("Type: R, ");
+    }
+    else if (opcode == 0x2 || opcode == 0x3) {
+        printf("Type: J, ");
+    }
+    else {
+        printf("Type: I, ");
+    }
+}
+
 int main(int argc, char* argv[]) {
     printf("Hello World\n");
-    // 명령행에서 파일 이름을 인자로 전달하지 않았을 경우 오류 메시지를 출력하고 종료합니다.
+
     if (argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return -1;
     }
 
-    // 파일 포인터를 선언하고 명령행에서 전달된 파일을 읽기 모드로 엽니다.
     FILE* fp = fopen(argv[1], "rb");
-
-    // 파일이 제대로 열렸는지 확인합니다.
     if (fp == NULL) {
         perror("Error opening file");
         exit(0);
     }
 
-    // 파일을 읽고 각 바이트를 16진수 형식으로 출력합니다.
-    unsigned char buffer[4];  // 4 바이트씩 읽기 위한 버퍼
+    unsigned char buffer[4];
     size_t bytesRead;
-    int count = 0;
+    int cycle = 1;
+    int pc = 0;
+    char data[100][33]; // 이진 문자열을 저장하는 배열
+    int row = 0;
 
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), fp)) > 0) {
-        for (size_t i = 0; i < bytesRead; i++) {
-            printf("%02X", buffer[i]);
-        }
-        count++;
+        uint32_t instruction = (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3];
 
-        // 2개의 4바이트 블록을 읽고 나면 줄 바꿈을 추가합니다.
-        if (count % 1 == 0) {
-            printf("\n");
+        for (int i = 0; i < 32; i++) {
+            data[row][i] = (instruction & (1 << (31 - i))) ? '1' : '0';
         }
+        data[row][32] = '\0';
+
+        printf("32190192> Cycle: %d\n", cycle++);
+        printf("\t[Instruction Fetch] %08x  (PC=0x%08x)\n", instruction, pc);
+        printf("\tdata[%d]: %s\n", row, data[row]);
+        classifyInstruction(instruction); // 명령어 타입 분류 및 출력
+        row++;
+        pc += 4;
+        printf("\n");
     }
 
-
-    // 파일을 닫습니다.
     fclose(fp);
-
-
-
-
     return 0;
 }
