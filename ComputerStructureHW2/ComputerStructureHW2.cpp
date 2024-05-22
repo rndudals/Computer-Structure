a0594139b0f107680484e35dd347d53f0b8d2d4b
kooyeongmin <rn9dud0@naver.com>
2024-05-22 15:45:07 +0900

    ALU lw 진행중2

--- ComputerStructureHW2/ComputerStructureHW2.cpp
+++ ComputerStructureHW2/ComputerStructureHW2.cpp
@@ -9,7 +9,7 @@ char data[100][33]; // 이진 문자열을 저장하는 배열
 int pc = 0;
 char* cur_instruction; // 현재 instruction
 int Register[32];
-
+int space[4294967295];
 
 
 uint32_t opcode;
@@ -40,39 +40,11 @@ int ALUResult;  // ALU 연산 결과
 
 char* defineRegisterName(int n) {
     switch (n) {
-    case 0: return "zero";
-    case 1: return "at";
-    case 2: return "v0";
-    case 3: return "v1";
-    case 4: return "a0";
-    case 5: return "a1";
-    case 6: return "a2";
-    case 7: return "a3";
-    case 8: return "t0";
-    case 9: return "t1";
-    case 10: return "t2";
-    case 11: return "t3";
-    case 12: return "t4";
-    case 13: return "t5";
-    case 14: return "t6";
-    case 15: return "t7";
-    case 16: return "s0";
-    case 17: return "s1";
-    case 18: return "s2";
-    case 19: return "s3";
-    case 20: return "s4";
-    case 21: return "s5";
-    case 22: return "s6";
-    case 23: return "s7";
-    case 24: return "t8";
-    case 25: return "t9";
-    case 26: return "k0";
-    case 27: return "k1";
-    case 28: return "gp";
-    case 29: return "sp";
-    case 30: return "s8";
-    case 31: return "ra";
-    default: return "unknown"; // 알 수 없는 레지스터 번호
+    case 0: return "zero"; case 1: return "at"; case 2: return "v0"; case 3: return "v1"; case 4: return "a0"; case 5: return "a1"; case 6: return "a2";
+    case 7: return "a3";   case 8: return "t0"; case 9: return "t1"; case 10: return "t2"; case 11: return "t3"; case 12: return "t4"; case 13: return "t5";
+    case 14: return "t6";  case 15: return "t7"; case 16: return "s0"; case 17: return "s1"; case 18: return "s2"; case 19: return "s3"; case 20: return "s4";
+    case 21: return "s5";  case 22: return "s6"; case 23: return "s7";  case 24: return "t8"; case 25: return "t9"; case 26: return "k0"; case 27: return "k1";
+    case 28: return "gp";  case 29: return "sp"; case 30: return "s8";  case 31: return "ra"; default: return "unknown"; // 알 수 없는 레지스터 번호
     }
 }
 
@@ -114,22 +86,22 @@ void parseRType(uint32_t instruction) {
     case 37: // move
     case 33: // addu
         printf("Inst: %s %s %s %s\n", "addu", defineRegisterName(rd), defineRegisterName(rs), defineRegisterName(rt));
-        printf("\t\topcode: %d, rd: %d (%x), rs: %d (%x), rt: %d (%x)\n", opcode, rd, rd, rs, rs, rt, rt);
+        printf("\t\topcode: %d, rd: %d (%x), rs: %d (%x), rt: %d (%x)\n", opcode, rd, Register[rd], rs, Register[rs], rt, Register[rt]);
         RegDst = 1; RegWrite = 1; ALUSrc = 0; PCSrc = 0; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 2;
         break;
     case 24: // mult
         printf("Inst: %s %s %s\n", "mult", defineRegisterName(rs), defineRegisterName(rt));
-        printf("\t\topcode: %d, rs: %d (%x), rt: %d (%x)\n", opcode, rs, rs, rt, rt);
+        printf("\t\topcode: %d, rs: %d (%x), rt: %d (%x)\n", opcode, rs, Register[rs], rt, Register[rt]);
         RegDst = 0; RegWrite = 1; ALUSrc = 0; PCSrc = 0; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 3;
         break;
     case 18: // mflo
         printf("Inst: %s %s\n", "mflo", defineRegisterName(rd));
-        printf("\t\topcode: %d, rd: %d (%x)\n", opcode, rd, rd);
+        printf("\t\topcode: %d, rd: %d (%x)\n", opcode, rd, Register[rd]);
         RegDst = 1; RegWrite = 1; ALUSrc = 0; PCSrc = 0; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 4;
         break;
     case 8: // jr
         printf("Inst: %s %s\n", "jr", defineRegisterName(rs));
-        printf("\t\topcode: %d, rs: %d (%x)\n", opcode, rs, rs);
+        printf("\t\topcode: %d, rs: %d (%x)\n", opcode, rs, Register[rs]);
         RegDst = 0; RegWrite = 0; ALUSrc = 0; PCSrc = 1; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 0;
         break;
     default:
@@ -164,22 +136,22 @@ void parseIType(uint32_t instruction) {
     switch (opcode) {
     case 9:
         printf("Inst: %s %s %s %d\n", "addiu", defineRegisterName(rt), defineRegisterName(rs), immediate);
-        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, rt, rs, rs, immediate);
+        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, Register[rt], rs, Register[rs], immediate);
         RegDst = 0; RegWrite = 1; ALUSrc = 1; PCSrc = 0; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 2;
         break;
     case 43: // sw
         printf("Inst: %s %s %d(%s)\n", "sw", defineRegisterName(rt), immediate, defineRegisterName(rs));
-        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, rt, rs, rs, immediate);
+        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, Register[rt], rs, Register[rs], immediate);
         RegDst = 0; RegWrite = 0; ALUSrc = 1; PCSrc = 0; MemRead = 0; MemWrite = 1; MemtoReg = 0; ALUOp = 2;
         break;
     case 35: // lw
         printf("Inst: %s %s %d(%s)\n", "lw", defineRegisterName(rt), immediate, defineRegisterName(rs));
-        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, rt, rs, rs, immediate);
+        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, Register[rt], rs, Register[rs], immediate);
         RegDst = 0; RegWrite = 1; ALUSrc = 1; PCSrc = 0; MemRead = 1; MemWrite = 0; MemtoReg = 1; ALUOp = 2;
         break;
     case 10: // slti
         printf("Inst: %s %s %s %d\n", "slti", defineRegisterName(rt), defineRegisterName(rs), immediate);
-        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, rt, rs, rs, immediate);
+        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, Register[rt], rs, Register[rs], immediate);
         RegDst = 0; RegWrite = 1; ALUSrc = 1; PCSrc = 0; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 7; // SLTI specific operation code
         break;
     case 5: // bnez or bne
@@ -189,7 +161,7 @@ void parseIType(uint32_t instruction) {
         else {
             printf("Inst: %s %s %s %d\n", "bne", defineRegisterName(rs), defineRegisterName(rt), immediate);
         }
-        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, rt, rs, rs, immediate);
+        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, Register[rt], rs, Register[rs], immediate);
         RegDst = 0; RegWrite = 0; ALUSrc = 0; PCSrc = 1; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 6; // Branch specific operation
         break;
     case 4: // b or beqz
@@ -199,7 +171,7 @@ void parseIType(uint32_t instruction) {
         else {
             printf("Inst: %s %s %d\n", "beqz", defineRegisterName(rs), immediate);
         }
-        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, rt, rs, rs, immediate);
+        printf("\t\topcode: %d, rt: %d (%x), rs: %d (%x), imm: %d\n", opcode, rt, Register[rt], rs, Register[rs], immediate);
         RegDst = 0; RegWrite = 0; ALUSrc = 0; PCSrc = 1; MemRead = 0; MemWrite = 0; MemtoReg = 0; ALUOp = 6; // Branch specific operation
         break;
     default:
@@ -291,7 +263,7 @@ void execute() {
     int readData1 = Register[rs];
     int readDaat2 = Register[rt];
 
-    printf("\t[Execute]\n");
+    printf("\t[Execute]");
     if (ALUSrc == 1) {
 
     }
@@ -303,21 +275,22 @@ void execute() {
     case 2: // addu(33), addiu(9), sw(43), lw(35)  -> add
         switch (opcode)
         {
+        case 37:
         case 33: // addu(33)
             ALUResult = Register[rs] + Register[rt];
-
+            printf(" ALU = %d\n", ALUResult);
             break;
         case 9: // addiu(9)
             ALUResult = Register[rs] + immediate;
-
+            printf(" ALU = %d\n", ALUResult);
             break;
         case 43: // sw(43)
             ALUResult = Register[rs] + immediate;
-
+            printf(" ALU = %d\n", ALUResult);
             break;
         case 35: // lw(35)
             ALUResult = Register[rs] + immediate;
-
+            printf(" ALU = %d\n", ALUResult);
             break;
         default:
             break;
@@ -339,6 +312,7 @@ void execute() {
 
     default: break;
     }
+    printf("\n");
 }
 
 void memoryAccess() {
@@ -346,7 +320,7 @@ void memoryAccess() {
         printf("\t[Memory Access] Load, Address: , Value: \n");
     }
     else if (MemWrite == 1) { // store일 때만 1
-        printf("\t[Memory Access] Store, Address: 0x%08x, Value: %d\n", ALUResult, ALUResult);
+        printf("\t[Memory Access] Store, Address: 0x%08x, Value: %d\n", ALUResult, Register[rt]);
     }
     else {
         printf("\t[Memory Access] Pass\n");
@@ -356,14 +330,14 @@ void memoryAccess() {
 void writeBack() {
     printf("\t[Write Back]");
     if (MemtoReg == 1) { // load일 때만 1
-        printf(" target: %d, Value: 0x%08x", Register[rt], Register[rt]);
+        printf(" target: %s, Value: 0x%08x", defineRegisterName(rt), Register[rt]);
     }
 
     if (RegWrite == 1) { //addu mult mflo lw addiu slti : 1
         printf(" TODO");
         switch (opcode)
         {
-        case 9:
+        case 9: // addiu
             Register[rt] = ALUResult;
             printf(" Target: %s, Value: %d / ", defineRegisterName(rt), ALUResult);
             break;
